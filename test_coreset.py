4f7c2386e8eb83b28632bebd2c7ec5578a731804
veaslee <veaslee@gmail.com>
2022-04-20 16:45:38 +0800

    m

--- test_coreset.py
+++ test_coreset.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='cifar10', help='dataset')
-parser.add_argument('--sample_size_frac', type=float, default=1.0, help='dataset')
+parser.add_argument('--sample_size_frac', type=float, default=1.0, help='dataset') # 0.04  0.1  0.2  0.4  1.0
 parser.add_argument('--batch_size', type=int, default=128, help='batch size')
 parser.add_argument('--load_path', type=str, default='../coreset_models/', help='iteration index')
 args = parser.parse_args()
@@ -24,15 +24,22 @@ sample_size = int(sample_size_frac*50000)
 corese_index_path = '../run/' + dataset + '/' + str(sample_size_frac) + '/' + str(sample_size) + '_selected.index'
 coreset_index = load_coreset_index(corese_index_path)
 
+print(len(core_index))
+
+print(max(core_index))
+
+print(min(core_index))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 use_cuda = torch.cuda.is_available()
 
-
+"""
 # Compute coreset complement risk
 for frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
     coreset_complement, _ = load_cifar_coreset_complement(dataset, batch_size, coreset_index, frac)
     risk_list = []
+
+
     for index in range(10):
         net = torch.load(load_path + 'resnet_' + dataset + '_sample_size' + str(sample_size) + '_frac_' + str(frac) + '_index_' + str(index+1))
         _, acc = test(net, coreset_complement)
@@ -41,3 +48,4 @@ for frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
 
     print('frac: ' + str(frac))
     print('coreset complement risk: ' + str(np.mean(risk_list)))
+"""
